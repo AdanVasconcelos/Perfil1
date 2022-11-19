82d04d4309164a9dc486e0025cf8dcee3b737d5b
Adan Vasconcelos <99154601+AdanVasconcelos@users.noreply.github.com>
2022-11-18 21:10:30 -0300

    Atualiscrip

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 function carregar(){
 var msgi = window.document.getElementById('msg');
+
 var data = new Date();
 var hora = data.getHours();
 var min = data.getMinutes();
 var seg = data.getSeconds();
-msgi.innerHTML = 'Agora são:'+hora+' hrs ' +min+ ' min e '+seg+' seg'
+
+msgi.innerHTML = 'Hours: '+hora+' hrs ' +min+ ' min e '+seg+' seg'
 
 
 }
